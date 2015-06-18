69b9c0e6a23c4d865123a55f619a6754bcc8e181
Josh Bodah <jb3689@yahoo.com>
2015-06-18 18:52:51 -0400

    actually add the reporter code...

--- lib/minitest/reporters/pride_reporter.rb
+++ lib/minitest/reporters/pride_reporter.rb
@@ -1,1 +1,27 @@
 require 'minitest/reporters/pride_reporter/version'
+require 'minitest/reporters'
+require 'minitest/pride_plugin'
+
+module Minitest::Reporters
+  class PrideReporter < DefaultReporter
+    def initialize(options = {})
+      super
+      @pride_io = Minitest::PrideLOL.new($stdout)
+    end
+
+    def record(test)
+      # Bypass super and call super's super
+      self.class.superclass.superclass
+                .instance_method(:record).bind(self).call(test)
+
+      print "#{"%.2f" % test.time} = " if options[:verbose]
+
+      @pride_io.print(test.result_code)
+
+      if @fast_fail && (test.skipped? || test.failure)
+        puts
+        print_failure(test)
+      end
+    end
+  end
+end
